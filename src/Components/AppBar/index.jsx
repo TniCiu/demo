19ab72c5ffad660b89f1ad49b626d44dc274189f
TniCiu <tindoris29@gmail.com>
2024-10-18 21:56:16 +0700

    Create folder Componets

--- src/Components/AppBar/index.jsx
+++ src/Components/AppBar/index.jsx
@@ -3,7 +3,7 @@ import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Button from "@mui/material/Button"
 import Box from "@mui/material/Box"
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 const Appbar = () => {
   return (
     <AppBar
